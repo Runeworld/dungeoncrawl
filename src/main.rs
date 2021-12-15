4ead34db3396517f6786d340ecf707418b353ec7
Simon-A-P <simon.pfaus@web.de>
2021-12-14 22:18:45 -0500

    ncurses rendering / single char inputs

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,9 @@
+use ncurses::*; // watch for globs
+
 #[derive(Debug)]
 struct Gamestate {
     current_tick: u64,
+    input: Vec<i32>,
 }
 
 impl Gamestate {
@@ -10,20 +13,42 @@ impl Gamestate {
 }
 
 fn main() {
-    let mut gamestate = Gamestate { current_tick: 0 };
+    let mut gamestate = Gamestate {
+        current_tick: 0,
+        input: Vec::new(),
+    };
+    initscr();
     loop {
-        process_input();
-        update(&mut gamestate); // logic
-        render(&gamestate); // display
+        render(&gamestate);
+        process_input(&mut gamestate);
+        update(&mut gamestate);
     }
+    //endwin();
 }
 
-fn process_input() {}
+fn process_input(gamestate: &mut Gamestate) {
+    let first = getch();
+    if first == 27 {
+        // if the first value is esc
+        getch(); // skip the [
+        gamestate.input.push(match getch() {
+            65 => 65, // code for arrow up
+            68 => 68, // code for arrow down
+            67 => 67, // code for arrow right
+            66 => 66, // code for arrow left
+            _ => 0,
+        })
+    } else {
+        gamestate.input.push(first);
+    }
+}
 
 fn update(gamestate: &mut Gamestate) {
     gamestate.tick()
 }
 
 fn render(gamestate: &Gamestate) {
-    println!("{:?}", gamestate)
+    clear();
+    addstr(&format!("{:?}", gamestate));
+    refresh();
 }
