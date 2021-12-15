21a8859bd102472f74254a9a0f3a1dfcb8b2d300
Simon-A-P <simon.pfaus@web.de>
2021-12-14 19:12:49 -0500

    First simple game loop

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,29 @@
+#[derive(Debug)]
+struct Gamestate {
+    current_tick: u64,
+}
+
+impl Gamestate {
+    fn tick(&mut self) {
+        self.current_tick += 1
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let mut gamestate = Gamestate { current_tick: 0 };
+    loop {
+        process_input();
+        update(&mut gamestate); // logic
+        render(&gamestate); // display
+    }
+}
+
+fn process_input() {}
+
+fn update(gamestate: &mut Gamestate) {
+    gamestate.tick()
+}
+
+fn render(gamestate: &Gamestate) {
+    println!("{:?}", gamestate)
 }
