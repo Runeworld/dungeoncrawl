995fdea3292954f3dd4718578a3b2a265997ea55
Simon-A-P <simon.pfaus@web.de>
2021-12-26 21:18:32 -0500

    Try out different code organization; Practice working with queries;

--- src/main.rs
+++ src/main.rs
@@ -1,47 +1,57 @@
 use bevy::prelude::*;
+use names::Generator;
+
+const WORLD_POPULATION: u32 = 100;
 
 fn main() {
     App::build()
         .add_plugins(DefaultPlugins)
-        .add_plugin(HelloPlugin)
+        .add_plugin(WorldSetupPlugin)
+        .add_plugin(RegisterSystemsPlugins)
         .run();
 }
 
 /* START Plugins */
-struct HelloPlugin;
-impl Plugin for HelloPlugin {
+struct WorldSetupPlugin;
+impl Plugin for WorldSetupPlugin {
+    fn build(&self, app: &mut AppBuilder) {
+        app.add_startup_system(add_people.system());
+    }
+}
+
+struct RegisterSystemsPlugins;
+impl Plugin for RegisterSystemsPlugins {
     fn build(&self, app: &mut AppBuilder) {
         app.insert_resource(GreetTimer(Timer::from_seconds(2.0, true)))
-            .add_startup_system(add_people.system())
-            .add_system(greet_people.system());
+            .add_system(print_people.system());
     }
 }
 
 /* START Systems */
-
 fn add_people(mut commands: Commands) {
-    commands
-        .spawn()
-        .insert(PersonComponent)
-        .insert(FriendlyComponent)
-        .insert(NameComponent("Kyle".to_string()));
-
-    commands
-        .spawn()
-        .insert(PersonComponent)
-        .insert(NameComponent("Simon".to_string()));
+    let mut generator = Generator::default();
+    for n in 0..WORLD_POPULATION {
+        commands
+            .spawn()
+            .insert(PersonComponent)
+            .insert(NameComponent(generator.next().unwrap()))
+            .insert(AgeComponent(n))
+            .insert(LevelComponent(n));
+    }
 }
 
-fn greet_people(
+fn print_people(
     time: Res<Time>,
     mut timer: ResMut<GreetTimer>,
-    query: Query<&NameComponent, (With<PersonComponent>, With<FriendlyComponent>)>,
+    query: Query<(&NameComponent, &AgeComponent, &LevelComponent), With<PersonComponent>>,
 ) {
     // update our timer with the time elapsed since the last update
     // if that caused the timer to finish, we say hello to everyone
     if timer.0.tick(time.delta()).just_finished() {
-        for name in query.iter() {
+        for (name, age, level) in query.iter() {
             println!("hello {}!", name.0);
+            println!("age: {}", age.0);
+            println!("level: {}", level.0);
         }
     }
 }
@@ -52,4 +62,5 @@ struct GreetTimer(Timer);
 /* START Components */
 struct PersonComponent;
 struct NameComponent(String);
-struct FriendlyComponent;
+struct AgeComponent(u32);
+struct LevelComponent(u32);
