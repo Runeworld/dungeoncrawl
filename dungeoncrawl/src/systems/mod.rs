2a2b191d8ceea1a4d67739fd74e7d9ef231ec779
spfaus <simon.pfaus@web.de>
2022-05-20 01:04:20 -0400

    Reintroduce movable player via Legion ECS

--- dungeoncrawl/src/systems/mod.rs
+++ dungeoncrawl/src/systems/mod.rs
@@ -1,12 +1,16 @@
+mod entity_render;
 mod map_render;
 mod player_input;
 
 use crate::prelude::*;
+
+use self::entity_render::entity_render;
 
 // Todo: Rename ~get...
 pub fn build_scheduler() -> Schedule {
     Schedule::builder()
         .add_system(player_input::player_input_system())
         .add_system(map_render::map_render_system())
+        .add_system(entity_render::entity_render_system())
         .build()
 }
