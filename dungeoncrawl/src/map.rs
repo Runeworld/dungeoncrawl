db1a30082c73f3854d1ba5bdfb0e685a9a6c564a
spfaus <simon.pfaus@web.de>
2022-05-27 16:12:18 -0400

    Render before seen tiles with dark-grey tint

--- dungeoncrawl/src/map.rs
+++ dungeoncrawl/src/map.rs
@@ -1,5 +1,6 @@
 use crate::prelude::*;
-const NUM_TILES: usize = (SCREEN_WIDTH * SCREEN_HEIGHT) as usize; // TODO: Have screen size separate from map size?
+
+const NUM_TILES: usize = (SCREEN_WIDTH * SCREEN_HEIGHT) as usize;
 
 #[derive(Copy, Clone, PartialEq)]
 pub enum TileType {
@@ -7,29 +8,25 @@ pub enum TileType {
     Floor,
 }
 
-// TODO: Rename
 pub fn map_idx(x: i32, y: i32) -> usize {
     ((y * SCREEN_WIDTH) + x) as usize
 }
 
 pub struct Map {
     pub tiles: Vec<TileType>,
+    pub revealed_tiles: Vec<bool>,
 }
 
 impl Map {
     pub fn new() -> Self {
         Self {
             tiles: vec![TileType::Floor; NUM_TILES],
+            revealed_tiles: vec![false; NUM_TILES],
         }
     }
 
-    // TODO: Returns bool -> Should be called ~is_in_bounds
     pub fn in_bounds(&self, point: Point) -> bool {
         point.x >= 0 && point.x < SCREEN_WIDTH && point.y >= 0 && point.y < SCREEN_HEIGHT
-    }
-
-    pub fn can_enter_tile(&self, point: Point) -> bool {
-        self.in_bounds(point) && self.tiles[map_idx(point.x, point.y)] == TileType::Floor
     }
 
     pub fn try_idx(&self, point: Point) -> Option<usize> {
@@ -38,6 +35,10 @@ impl Map {
         } else {
             Some(map_idx(point.x, point.y))
         }
+    }
+
+    pub fn can_enter_tile(&self, point: Point) -> bool {
+        self.in_bounds(point) && self.tiles[map_idx(point.x, point.y)] == TileType::Floor
     }
 
     fn valid_exit(&self, loc: Point, delta: Point) -> Option<usize> {
@@ -66,6 +67,10 @@ impl Algorithm2D for Map {
 }
 
 impl BaseMap for Map {
+    fn is_opaque(&self, idx: usize) -> bool {
+        self.tiles[idx as usize] != TileType::Floor
+    }
+
     fn get_available_exits(&self, idx: usize) -> SmallVec<[(usize, f32); 10]> {
         let mut exits = SmallVec::new();
         let location = self.index_to_point2d(idx);
@@ -88,9 +93,5 @@ impl BaseMap for Map {
 
     fn get_pathing_distance(&self, idx1: usize, idx2: usize) -> f32 {
         DistanceAlg::Pythagoras.distance2d(self.index_to_point2d(idx1), self.index_to_point2d(idx2))
-    }
-
-    fn is_opaque(&self, idx: usize) -> bool {
-        self.tiles[idx as usize] != TileType::Floor
     }
 }
