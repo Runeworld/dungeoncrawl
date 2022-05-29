a3b928b9bcd79bd178feaeb19dca74a686b91b6a
spfaus <simon.pfaus@web.de>
2022-05-29 16:43:16 -0400

    Add current dungeon level to HUD

--- dungeoncrawl/src/systems/hud.rs
+++ dungeoncrawl/src/systems/hud.rs
@@ -12,7 +12,7 @@ pub fn hud(ecs: &SubWorld) {
 
     let mut draw_batch = DrawBatch::new();
     draw_batch.target(2);
-    draw_batch.print_centered(1, "Explore the Dungeon. Arrow keys to move.");
+    draw_batch.print_centered(1, "Explore the Dungeon. Cursor keys to move.");
     draw_batch.bar_horizontal(
         Point::zero(),
         SCREEN_WIDTH * 2,
@@ -29,10 +29,17 @@ pub fn hud(ecs: &SubWorld) {
         ColorPair::new(WHITE, RED),
     );
 
-    let player = <(Entity, &Player)>::query()
+    let (player, map_level) = <(Entity, &Player)>::query()
         .iter(ecs)
-        .find_map(|(entity, _player)| Some(*entity))
+        .find_map(|(entity, player)| Some((*entity, player.map_level)))
         .unwrap();
+
+    draw_batch.print_color_right(
+        Point::new(SCREEN_WIDTH * 2, 1),
+        format!("Dungeon Level: {}", map_level + 1),
+        ColorPair::new(YELLOW, BLACK),
+    );
+
     let mut item_query = <(&Item, &Name, &Carried)>::query();
     let mut y = 3;
     item_query
