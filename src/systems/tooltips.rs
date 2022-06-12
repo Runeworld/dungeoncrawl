7bef14e2339f18e322421bfd92cf74d979ef145e
spfaus <simon.pfaus@web.de>
2022-06-12 02:01:21 -0400

    Refactor tooltips function

--- src/systems/tooltips.rs
+++ src/systems/tooltips.rs
@@ -5,8 +5,12 @@ use crate::prelude::*;
 #[read_component(Name)]
 #[read_component(FieldOfView)]
 #[read_component(Player)]
-pub fn tooltips(ecs: &SubWorld, #[resource] mouse_pos: &Point, #[resource] camera: &Camera) {
-    let mut positions = <(Entity, &Point, &Name)>::query();
+pub fn tooltips_render_name(
+    ecs: &SubWorld,
+    #[resource] mouse_pos: &Point,
+    #[resource] camera: &Camera,
+) {
+    let mut positions = <(&Point, &Name)>::query();
     let mut fov = <&FieldOfView>::query().filter(component::<Player>());
     let offset = Point::new(camera.left_x, camera.top_y);
     let map_pos = *mouse_pos + offset;
@@ -15,15 +19,10 @@ pub fn tooltips(ecs: &SubWorld, #[resource] mouse_pos: &Point, #[resource] camer
     let player_fov = fov.iter(ecs).next().unwrap();
     positions
         .iter(ecs)
-        .filter(|(_, pos, _)| **pos == map_pos && player_fov.visible_tiles.contains(pos))
-        .for_each(|(entity, _, name)| {
+        .filter(|(pos, _)| **pos == map_pos && player_fov.visible_tiles.contains(pos))
+        .for_each(|(_, name)| {
             let screen_pos = *mouse_pos * 4;
-            let display =
-                if let Ok(health) = ecs.entry_ref(*entity).unwrap().get_component::<Health>() {
-                    format!("{} : {} hp", &name.0, health.current)
-                } else {
-                    name.0.clone()
-                };
+            let display = name.0.clone();
             draw_batch.print(screen_pos, &display);
         });
     draw_batch.submit(Z_INDEX_TOOLTIPS).expect("Batch error");
